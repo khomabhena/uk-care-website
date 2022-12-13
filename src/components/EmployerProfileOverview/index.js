5e3cfce54511f4174f1f8beaf08851aace60a116
khomabhena <colwanymab@gmail.com>
2022-12-13 18:46:58 +0200

    Factoring out useless code

--- src/components/EmployerProfileOverview/index.js
+++ src/components/EmployerProfileOverview/index.js
@@ -1,34 +1,19 @@
-import React, { useContext, useState } from 'react'
-import { AuthContext } from '../Context/AuthContext';
-import { CompanyContext } from '../Context/CompanyContext';
+import React, { useState } from 'react'
 import EmployerJobs from '../EmployerJobs';
-import { FileChooser, Input, InputContainer, InputLabel, InputWrap, TextArea, UpdateForm } from '../EmployerProfileUpdate/EmployerProfileUpdateElements';
-import JobDetails from '../JobDetails';
-import Jobs from '../Jobs';
-import { JobsContainer } from '../Profile/ProfileElements';
-import {  SubTitle } from '../ProfileInfo/ProfileInfoElements'
-import { Title, Text, EducationContainer, EducationDetailsWrap, EducationWrap, Language, Letter, LetterWrap, OverviewWrap, Skill, SkillsContainer, SkillsWrap, JobTitle, JobContainer, HorizontalWrap, NewJob, CreateJobContainer, UpdateButton } from './EmployerProfileOverviewElements'
+import { Input, InputContainer, InputLabel, InputWrap, TextArea, UpdateForm } from '../EmployerProfileUpdate/EmployerProfileUpdateElements';
+import { Title, Text, OverviewWrap, JobTitle, JobContainer, HorizontalWrap, NewJob, CreateJobContainer, UpdateButton } from './EmployerProfileOverviewElements'
 
 
 const EmployerProfileOverview = ({ selectedProfile }) => {
 
-    const about = "Provider of hospice services. The company's services include nursing visits, medical professional assistance, pain and symptom management, medications, equipment and supplies, spiritual counseling and bereavement follow up, and home health aide services, enabling chronically ill patients to get over their emotional and spiritual needs.\n\n" +
-                  "Industries in the Nursing and Residential Care Facilities subsector provide residential care combined with either nursing, supervisory, or other types of care as required by the residents. In this subsector, the facilities are a significant part of the production process, and the care provided is a mix of health and social services with the health services being largely some level of nursing services.";
+    // const about = "Provider of hospice services. The company's services include nursing visits, medical professional assistance, pain and symptom management, medications, equipment and supplies, spiritual counseling and bereavement follow up, and home health aide services, enabling chronically ill patients to get over their emotional and spiritual needs.\n\n" +
+    //               "Industries in the Nursing and Residential Care Facilities subsector provide residential care combined with either nursing, supervisory, or other types of care as required by the residents. In this subsector, the facilities are a significant part of the production process, and the care provided is a mix of health and social services with the health services being largely some level of nursing services.";
 
-    const { companyData } = useContext(CompanyContext);
-    const { userUid } = useContext(AuthContext)
 
     const [createJob, setCreateJob] = useState(false);
     const [buttonName, setButtonName] = useState('Create new Job');
     const [title, setTitle] = useState('Current Openings');
-    const [jobPost, setJobPost] = useState({
-      title: '',
-      salary: '',
-      location: '',
-      type: '',
-      description: '',
-      company: ''
-    })
+  
 
     const handleCreateJob = () => {
         setCreateJob(!createJob)
@@ -36,16 +21,10 @@ const EmployerProfileOverview = ({ selectedProfile }) => {
         createJob ? setTitle('Current Openings') : setTitle('Create new Job')
     }
 
-    const uploadJob = (userUid, companyName) => {
-        console.log(userUid)
-        console.log(companyName)
-    } 
+   
 
     const handleJobPost = (e) => {
       e.preventDefault()
-      setJobPost(prev => ({...prev, company: companyData.companyName}))
-      // console.log(jobPost)
-      uploadJob(userUid, companyData.companyName)
     }
 
   return (
@@ -69,26 +48,26 @@ const EmployerProfileOverview = ({ selectedProfile }) => {
               <InputContainer>
                   <InputWrap>
                       <InputLabel>Job Title</InputLabel>
-                      <Input placeholder="What's the job title"  onChange={(e) => setJobPost(prev => ({...prev, title: e.target.value}))}/>
+                      <Input placeholder="What's the job title" />
                   </InputWrap>
                   <InputWrap>
                       <InputLabel>Expected Salary per Month</InputLabel>
-                      <Input placeholder='Enter the expected salary per month' onChange={(e) => setJobPost(prev => ({...prev, salary: e.target.value}))} />
+                      <Input placeholder='Enter the expected salary per month' />
                   </InputWrap>
               </InputContainer>
               <InputContainer>
                   <InputWrap>
                       <InputLabel>Location</InputLabel>
-                      <Input placeholder='Where is the job located?' onChange={(e) => setJobPost(prev => ({...prev, location: e.target.value}))} />
+                      <Input placeholder='Where is the job located?' />
                   </InputWrap>
                   <InputWrap>
                       <InputLabel>Job Type</InputLabel>
-                      <Input placeholder='Full Time or Part Time' onChange={(e) => setJobPost(prev => ({...prev, type: e.target.value}))} />
+                      <Input placeholder='Full Time or Part Time' />
                   </InputWrap>
               </InputContainer>
               
               <InputLabel>Job Description</InputLabel>
-              <TextArea placeholder='Write a short paragraph describing the job' rows='6' onChange={(e) => setJobPost(prev => ({...prev, description: e.target.value}))} />
+              <TextArea placeholder='Write a short paragraph describing the job' rows='6' />
               <UpdateButton type='submit'>Create new Job</UpdateButton>
             </UpdateForm>
           </CreateJobContainer>
