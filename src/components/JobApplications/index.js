a8963ca5c8e837ec3d8fdc896b344de20685e853
khomabhena <colwanymab@gmail.com>
2022-12-21 07:53:51 +0200

    Created Job Applications for Applicant

---
+++ src/components/JobApplications/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { ApplyButton, BottomWrap, CompanyName, Date, HorizontalLine, JobDescription, JobType, JobWrapper, Salary, SalaryWrap, Title } from './JobApplicationElements'
+
+const JobApplications = ({ selectedJobPage, toggleJobPage }) => {
+    const duties = "Certified Nursing Assistants help perform critical tasks that ensure patients are well-cared for and safe during their hospital stay. Checking vital signs, keeping patients clean, and other duties like this are extremely important for patient care and satisfaction."
+
+  return (
+    <JobWrapper selectedJobPage={selectedJobPage}>
+        <Title>Assistant Nurse Aid</Title>
+        <CompanyName>UK Care Connections</CompanyName>
+        <SalaryWrap>
+            <Salary>$500/Month</Salary>
+            <JobType>Nurse Aid</JobType>
+        </SalaryWrap>
+        <JobDescription>{duties}</JobDescription>
+        <HorizontalLine />
+        <BottomWrap>
+            <Date>2 days ago</Date>
+            <ApplyButton onClick={() => toggleJobPage('jobDetails')}>Cancel</ApplyButton>
+        </BottomWrap>
+    </JobWrapper>
+  )
+}
+
+export default JobApplications
